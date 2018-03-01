bb9b572a48443cf5655386fd104eb731bcc34931
edaineko <daineko.ekaterina@gmail.com>
2018-03-01 14:15:39 +0300

    additional_5 solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    if (str.length % 2 !== 0)
+        return false;
+
+    var arr = [],
+        s = 0;
+
+    for (var i = 0; i < bracketsConfig.length; i++) {
+        arr[i] = bracketsConfig[i].join('');
+    }
+
+    while ((str.length > 1) && (s < str.length)) {
+        var bracketsTemp = str.substring(s, s + 2);
+
+        if (arr.includes(bracketsTemp)) {
+            str = str.replace(bracketsTemp, '');
+            s = 0;
+
+        } else {
+            s++;
+        }
+    }
+
+    if (str.length === 0){
+        return true;
+
+    } else {
+        return false;
+    }
 }
